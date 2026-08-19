e97e1ec94ee89f4252fd1b33e487ce99fc13aa8c
Aluno <e023aluno@outlook.com>
2026-08-18 21:57:01 -0300

    ListaProdutos feito!!

--- frontend/src/components/ListaProdutos.jsx
+++ frontend/src/components/ListaProdutos.jsx
@@ -1,22 +1,38 @@
 import Produto from "./Produto";
 
+function ListaProdutos({ produtos, busca }){
 
-export default function ListaProduto({ produtos}){
-    if(produtos.length === 0){
-        return <p>Nenhum produto cadastrado</p>;
+    if (produtos.length === 0){
+
+        return(
+            <section className="estado-vazio">
+                <span className="icone-vazio">🔎</span>
+                <h2>{busca ? "Nenhum produto encotrado" : "Nenhum produto cadastrado "} </h2>
+                <p>
+                    {
+                        busca
+                        ? "Tente pesquisar usando outro nome."
+                        : "Cadastre o primeiro produto para iniciar o catálogo."
+                    }
+                </p>
+            </section>
+        )
     }
-
     return(
-        <section>
-            <h2 className="titulo-secao">Produtos cadastrados</h2>
-
+        <section className="secao-produtos">
+            <div className="cabecalho-lista">
+                <div>
+                    <span className="tag">INVERTÁRIO</span>
+                    <h2 className="titulo-secao">Produtos cadastrados</h2>
+                </div>
+                <span className="resultado-lista">{produtos.length}Exibido(s)</span>
+            </div>
             <div className="grid">
                 {produtos.map((produto) => (
-                <Produto key={produto.id} produto={produto}/>
-                
+                    <Produto key={produto.id} produto={produto} />
                 ))}
-
             </div>
         </section>
     );
 }
+export default ListaProdutos;

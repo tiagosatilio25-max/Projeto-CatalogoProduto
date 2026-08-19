51cb08ccb0f31f8151a8e09dd6de543ab9f589ad
Aluno <e023aluno@outlook.com>
2026-08-18 22:10:07 -0300

    Header feito!!

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,10 +2,17 @@ export default function Header(){
     return(
 
         <header className="header">
-            
+            <div className="marca">
+                <span className="marca-icone">&lt;/&gt;</span>
+            <div>
             <h1>Catálogo de Produtos</h1>
             <p>Projeto Full Stack</p>
+                </div>
+                
+                <span className="header-badge">REACT FRONT-END </span>
+            </div>
         </header>
 
     );
 }
+export default Header;

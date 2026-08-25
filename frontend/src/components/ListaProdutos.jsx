9646b37e6d795e4271c07831eb364cade52b598b
Aluno <e023aluno@outlook.com>
2026-08-25 20:22:54 -0300

    ListaProdutos feito

--- frontend/src/components/ListaProdutos.jsx
+++ frontend/src/components/ListaProdutos.jsx
@@ -1,38 +1,62 @@
 import Produto from "./Produto";
 
-function ListaProdutos({ produtos, busca }){
+// ALTERADO: recebendo aoEditar e aoExcluir
+function ListaProdutos({ produtos, busca, aoEditar, aoExcluir }) {
 
-    if (produtos.length === 0){
 
-        return(
-            <section className="estado-vazio">
-                <span className="icone-vazio">🔎</span>
-                <h2>{busca ? "Nenhum produto encotrado" : "Nenhum produto cadastrado "} </h2>
-                <p>
-                    {
-                        busca
-                        ? "Tente pesquisar usando outro nome."
-                        : "Cadastre o primeiro produto para iniciar o catálogo."
-                    }
-                </p>
-            </section>
-        )
-    }
+  if (produtos.length === 0) {
+   
     return(
-        <section className="secao-produtos">
-            <div className="cabecalho-lista">
-                <div>
-                    <span className="tag">INVERTÁRIO</span>
-                    <h2 className="titulo-secao">Produtos cadastrados</h2>
-                </div>
-                <span className="resultado-lista">{produtos.length}Exibido(s)</span>
-            </div>
-            <div className="grid">
-                {produtos.map((produto) => (
-                    <Produto key={produto.id} produto={produto} />
-                ))}
-            </div>
-        </section>
-    );
+      <section className="estado-vazio">
+
+           <span className="icone-vazio">🔎</span> 
+
+           <h2>{busca ? "Nenhum produto encontrado" : " nenhum produto cadastrado  "} </h2>
+
+            <p>
+              {
+                busca
+                ? "Tente pesquisar usando outro nome."
+                : "Cadastre o primeiro produto para iniciar o catálogo."
+              }
+            </p>
+
+      </section>
+    )
+  }
+
+  return (
+    <section className="secao-produtos">
+      
+      <div className="cabecalho-lista">
+
+        <div>
+
+          <span className="tag">INVENTÁRIO</span>
+          <h2 className="titulo-secao">Produtos cadastrados</h2>
+
+        </div>
+          <span className="resultado-lista"> {produtos.length} exibido(s)</span>
+
+
+      </div>
+
+
+      <div className="grid">
+        {produtos.map((produto) => (
+          // ALTERADO: repassando ações para cada card
+          <Produto
+            key={produto.id}
+            produto={produto}
+            aoEditar={aoEditar}
+            aoExcluir={aoExcluir}
+          />
+        ))}
+
+
+      </div>
+    </section>
+  );
 }
+
 export default ListaProdutos;
